fe4d4b2778e0c4fc4275fa115cb0c3f32976faaf
kevinjones557 <44711717+kevinjones557@users.noreply.github.com>
2024-02-14 15:18:52 -0500

    Google Sign in works
    Next steps:
     - sync if possible
     - database connection

--- time-capsule-frontend/Screens/Login.js
+++ time-capsule-frontend/Screens/Login.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { Platform, StyleSheet, TouchableOpacity, Text,Image } from "react-native";
 import GoogleLogo from '../icons/google-.png';
 import GreenBackground from "../Components/GreenBackground";
 import * as Google from "expo-auth-session/providers/google";
+import base64 from 'react-native-base64';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 //const [userInfo, setUserInfo] = useState(null);
 
@@ -36,26 +37,28 @@ const Login = ({ navigation }) => {
       React.useEffect(() => {
         if (response?.type === 'success') {
           const { authentication } = response;
+          const idToken = response.authentication.idToken;
+
+          const tokenParts = idToken.split('.');
+          const encodedPayload = tokenParts[1];
+
+          const decodedPayload = base64.decode(encodedPayload);
+
+          const payload = JSON.parse(decodedPayload.replace(/[\u0000-\u001F\u007F-\u009F]/g, ''));
+
+          const userEmail = payload.email;
+          console.log('reponse', userEmail);
+          navigation.navigate('Registration');
+
         }
       }, [response]);
     
-      console.log('reponse', response)
-    //   React.useEffect(() => {
-    //     // Handle authentication response
-    //     if (response?.type === 'success') {
-    //       const { id_token } = response.params;
-    //       // Store the ID token in AsyncStorage or wherever you want
-    //       AsyncStorage.setItem('google_id_token', id_token);    
-    //     }
-    //   }, [response]);
-    //   console.log(response)
 	return (
         <GreenBackground>
-            <TouchableOpacity style={styles.container1} onPress={() => navigation.navigate('Registration')} title=''>
-                <Text style={styles.text}>To Register</Text>
-            </TouchableOpacity>
+            <Image style={styles.capsule} source={require('../icons/capsule-.png')} />
             <TouchableOpacity style={styles.container} onPress={() => promptAsync()} title=''>
                 <Text style={styles.text}>Sign in with Google</Text>
+                <Image style={styles.google} source={require('../icons/google-.png')} />
             </TouchableOpacity>
         </GreenBackground>
 	);
@@ -84,7 +87,7 @@ const styles = StyleSheet.create({
     },
     text: {
         position: 'absolute',
-        left: '35%',
+        left: '25%',
         top: '10%',
         fontStyle: 'normal',
         fontWeight: '400',
@@ -94,14 +97,28 @@ const styles = StyleSheet.create({
         color: '#000000',
     },
     google: {
-        backgroundImage: `url(${GoogleLogo})`,
+        position: 'absolute',
+        width: 35,
+        height: 36,
+        left: '3%',
+        top: '13%',
       },
+    capsule: {
+        position: 'absolute',
+        width: 425,
+        height: 183,
+        left: -18,
+        top: 56,
+        ...Platform.select({
+            ios: {
+              shadowColor: '#000',
+              shadowOffset: { width: 0, height: 4 },
+              shadowOpacity: 0.5,
+              shadowRadius: 4,
+            },
+            android: {
+              elevation: 4,
+            },
+          }),
+    }
 });
-
-
-
-
-
-
-
-
