e6965a0a18901d1cef4c4cb7aa7ca0e635e27678
kevinjones557 <44711717+kevinjones557@users.noreply.github.com>
2024-02-13 23:52:51 -0500

    A good start
    A good start to the sign in process.

--- time-capsule-frontend/Screens/Login.js
+++ time-capsule-frontend/Screens/Login.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
 import GoogleLogo from '../icons/google-.png';
 import GreenBackground from "../Components/GreenBackground";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+//const [userInfo, setUserInfo] = useState(null);
+
+//client IDs from .env
+// const config = {
+//   androidClientId: Config.ANDROID_CLIENT_ID,
+//   iosClientId: Config.IOS_CLIENT_ID,
+//   webClientId: Config.WEB_CLIENT_ID,
+// };
+
+//const [request, response, promptAsync] = Google.useAuthRequest(config);
+
+
 
 const Login = ({ navigation }) => {
-    return (
+
+    const ANDROID_CLIENT_ID='457222296603-f24r4dn3je9vsctojduvh8a55f01dcf5.apps.googleusercontent.com'
+    const IOS_CLIENT_ID='457222296603-d9o7m2le03mov1r3h3o4ettlodekdkp9.apps.googleusercontent.com'
+    const WEB_CLIENT_ID='457222296603-r8cej7d4qa3420a2du8gv59p9qkavc54.apps.googleusercontent.com'
+
+    const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+        androidClientId: ANDROID_CLIENT_ID,
+        iosClientId: IOS_CLIENT_ID,
+        webClientId: WEB_CLIENT_ID,
+        // NOTE: For iOS, specify the scopes required
+        scopes: ['email'],
+      });
+    
+      React.useEffect(() => {
+        // Handle authentication response
+        if (response?.type === 'success') {
+          const { id_token } = response.params;
+          // Store the ID token in AsyncStorage or wherever you want
+          AsyncStorage.setItem('google_id_token', id_token);    
+        }
+      }, [response]);
+      console.log(response)
+	return (
         <GreenBackground>
-            <TouchableOpacity style={styles.container} onPress={() => navigation.navigate('Registration')} title=''>
+            <TouchableOpacity style={styles.container1} onPress={() => navigation.navigate('Registration')} title=''>
+                <Text style={styles.text}>To Register</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.container} onPress={() => promptAsync()} title=''>
                 <Text style={styles.text}>Sign in with Google</Text>
             </TouchableOpacity>
         </GreenBackground>
-    );  
+	);
 }
 
 export default Login;
@@ -22,6 +61,15 @@ const styles = StyleSheet.create({
         height: 50,
         left: 47,
         top: 695, 
+        backgroundColor: 'rgba(255, 255, 255, 0.48)',
+        borderRadius: 10,
+    },
+    container1: {
+        position: 'absolute',
+        width: 296,
+        height: 50,
+        left: 47,
+        top: 495, 
         backgroundColor: 'rgba(255, 255, 255, 0.48)',
         borderRadius: 10,
     },
