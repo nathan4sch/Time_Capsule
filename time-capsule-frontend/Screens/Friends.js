c7239b4c4214058f81997e9770480d44f60e9a1a
PJ Henwood <pjhenwood8@gmail.com>
2024-02-18 17:55:54 -0500

    Initial Friends Page Finished
    The initial friend's page is now done.

--- time-capsule-frontend/Screens/Friends.js
+++ time-capsule-frontend/Screens/Friends.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import {View, Image, Text, TextInput, TouchableOpacity, FlatList } from "react-native";
 import BlackBackground from "../Components/BlackBackground";
+import { commonStyles } from "../Components/FriendsPageStylings";
 import { useGlobalContext } from "../context/globalContext";
-import { commonStyles } from "../Components/CommonStyles";
 
 const Friends = ({ navigation }) => {
     const { curUser, getUserbyID } = useGlobalContext();
@@ -23,6 +23,7 @@ const Friends = ({ navigation }) => {
         fetchData();
     }, []);
 
+    
     const renderFriendRequestsItem = ({ item }) => (
         <View style={commonStyles.listItemContainer}>
             <Text style={commonStyles.usernameText}>{item}</Text>
@@ -46,17 +47,24 @@ const Friends = ({ navigation }) => {
 
     return (
         <BlackBackground>
+
             <TouchableOpacity style={commonStyles.backButtonTop} onPress={() => navigation.navigate('TempMain')}>
-        {/*Fix Arrow */}
                 <View style={commonStyles.arrowContainer}>
-                <View style={commonStyles.arrowLine} />
-                <View style={[commonStyles.arrowLine, { transform: [{ rotate: '180deg' }] }]} />
+                <Image style={commonStyles.arrowIconContainer} source={require('../icons/tempbackarrow-.png')} />
                 </View>
             </TouchableOpacity>
 
+            <View style={commonStyles.searchContainer }>
+                <View style={commonStyles.searchBar} />
+                <Image style={commonStyles.searchIconContainer} source={require('../icons/search-.png')} />
+                <TextInput style={commonStyles.searchTextContainer} placeholder="Type here..."/>
+            </View>
+
+
             <View style={commonStyles.listContainer }>
-            <View style={commonStyles.line} />
+
             <Text style={commonStyles.title}>Friend Requests</Text>
+            <View style={commonStyles.line} />
             <FlatList
                 data={friendRequests}
                 keyExtractor={(item, index) => index.toString()}
@@ -64,9 +72,8 @@ const Friends = ({ navigation }) => {
                 ItemSeparatorComponent={() => <View style={commonStyles.separator} />}
             />
 
-
-
             <Text style={commonStyles.title}>Friends</Text>
+            <View style={commonStyles.line} />
             <FlatList
                 data={friendObj}
                 keyExtractor={(item, index) => index.toString()}
@@ -80,134 +87,3 @@ const Friends = ({ navigation }) => {
 };
 
 export default Friends;
-/*
-import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from "react-native";
-import BlackBackground from "../Components/BlackBackground";
-import { useGlobalContext } from "../context/globalContext";
-
-const Friends = ({ navigation }) => {
-    const { curUser, getUserbyID } = useGlobalContext();
-
-    const friendsID = curUser.friends; // objectids
-    const friendRequests = curUser.friendRequests; // contains usernames
-    const [friendObj, setFriendObj] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const promises = friendsID.map(async (id) => {
-                return await getUserbyID(id);
-            });
-
-            const friendData = await Promise.all(promises);
-            setFriendObj(friendData);
-        };
-
-        fetchData();
-    }, []);
-
-    // Render Item for Friend Requests
-    const renderFriendRequestsItem = ({ item }) => (
-        <View style={styles.listItemContainer}>
-            <Text style={styles.usernameText}>{item}</Text>
-        </View>
-    );
-
-    // Render Item for Usernames from Objects
-    const renderUsernamesItem = ({ item }) => (
-        <View style={styles.listItemContainer}>
-            <Text style={styles.usernameText}>{item.username}</Text>
-        </View>
-    );
-
-    return (
-        <BlackBackground>
-            <View style={styles.line}></View>
-            <View style={styles.container}>
-*/                {/* Friend Requests List */}
-/*                <Text style={styles.title}>Friend Requests</Text>
-                <FlatList
-                    data={friendRequests}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={renderFriendRequestsItem}
-                    ItemSeparatorComponent={() => <View style={styles.separator} />}
-                />
-*/
-                {/* Usernames from Objects List */}
-                /*<Text style={styles.title}>Usernames from Objects</Text>
-                <FlatList
-                    data={friendObj}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={renderUsernamesItem}
-                    ItemSeparatorComponent={() => <View style={styles.separator} />}
-                />
-            </View>
-*/
-            {/* Back Button */}
-            /*<TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('TempMain')}>
-                <Text style={styles.backButtonText}>Back</Text>
-            </TouchableOpacity>
-        </BlackBackground>
-    );
-};*/
-/*
-export default Friends;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 20,
-        paddingHorizontal: 20,
-    },
-
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        color: 'white',
-    },
-
-    listItemContainer: {
-        backgroundColor: '#2c3e50',
-        paddingVertical: 15,
-        paddingHorizontal: 20,
-        borderRadius: 8,
-        marginBottom: 10,
-    },
-    line: {
-        position: 'fixed',
-        width: 360,
-        height: 0,
-        left: 0,
-        top: 200,
-        borderBottomWidth: 1,
-        borderBottomColor: '#FFFFFF',
-    },
-
-    usernameText: {
-        fontSize: 16,
-        color: 'white',
-    },
-
-    separator: {
-        height: 1,
-        backgroundColor: '#34495e',
-        marginVertical: 10,
-    },
-
-    backButton: {
-        position: 'absolute',
-        bottom: 20,
-        left: 20,
-        backgroundColor: '#3498db',
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        borderRadius: 8,
-    },
-
-    backButtonText: {
-        fontSize: 16,
-        color: 'white',
-    },
-});
-*/
