125ec8c7d7d6340f55f091889cd73703fe6307bb
PJ Henwood <pjhenwood8@gmail.com>
2024-03-01 01:00:24 -0500

    Created History Page
    Created History Page

--- time-capsule-frontend/Screens/History.js
+++ time-capsule-frontend/Screens/History.js
@@ -3,83 +3,84 @@ import { StyleSheet, View, Text, Button, BackHandler, Pressable, FlatList, Touch
 import PageNavBar from "../Components/PageNavBar";
 import { commonStyles } from "../Components/FriendsPageStylings";
 import { useGlobalContext } from "../context/globalContext";
-import HistoryBackground from "../Components/HistoryBackground";
+import BlackBackground from "../Components/BlackBackground";
 
 const History = ({ navigation }) => {
-    const { setCurUser, curUser, getUserbyID, getUser, error } = useGlobalContext();
+    const { curUser, getCapsule, } = useGlobalContext();
     const [reloadApp, setReloadApp] = useState(false);
-
+    const [shownCapsule, setShownCapsule] = useState("");
 
     useEffect(() => {
-        const fetchData = async () => {
-            // Retrieve current user
-            const updatedUser = await getUserbyID(curUser._id);
-            setCurUser(updatedUser);
+        const getCapsuleFunc = async () => {
+            if (curUser.capsules.length !== 0) {
+                const capsule = await getCapsule(curUser.capsules[0]);
+                setShownCapsule(capsule.snapshot);
+            }
         };
+        getCapsuleFunc();
+    }, []);
 
-        fetchData();
-    }, [reloadApp]);
-
-    const renderCapsule = ({ item }) => (
-        // TODO 
-        //   We will finish implementing this method once we figure out how we plan on doing the capusle.
-        //   Right now this is just set to display the timestamp and snapshot, I don't have styling
-        //   for this yet since we don't have capsule data to test with.
-        <View style={commonStyles.listItemContainer}>
-            <Text style={commonStyles.usernameText}>{item.timestamp}</Text>
-            <Image style={commonStyles.icon} source={item.snapshot} />
-        </View>
-    );
+    const handleOverlayButtonPress = () => {
+        
+    };
     
     return (
-        <>
-        <PageNavBar onBackPress={() => navigation.goBack()} title="History"/>
-        
-        <View style={styles.capsuleContainer}>
-            {curUser.capsules.length > 0 ? (
-            <>
-            <FlatList
-                data={curUser.capsules}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={renderCapsule}
-                ItemSeparatorComponent={() => <View style={commonStyles.separator} />}
-            />
-            </>
-            ) : (
-            <>            
-            <Text style={styles.text}>No Capsules...</Text>
-            </>
-            )}
-        </View>
-        </>
+        <BlackBackground>
+            <PageNavBar onBackPress={() => navigation.goBack()} title="History Page" />
+            
+            <View style={styles.imageContainer}>
+                {shownCapsule ? (
+                    <Image
+                        style={styles.capsuleImage}
+                        source={{ uri: shownCapsule }}
+                    />
+                ) : (
+                    // Render something else when shownCapsule is empty
+                    <Text style={styles.overlayText}>No Capsule Available</Text>
+                )}
+                <TouchableOpacity style={styles.overlayButton} onPress={handleOverlayButtonPress}></TouchableOpacity>
+            </View>
+            <View style={styles.capsuleList} />
+        </BlackBackground>
     );  
 }
 
 export default History;
 
-const styles = StyleSheet.create({  
-    container: {
+const styles = StyleSheet.create({
+    imageContainer: {
         position: 'absolute',
-        width: 296,
-        height: 50,
-        left: 47,
-        top: 695, 
-        backgroundColor: 'rgba(255, 255, 255, 0.48)',
-        borderRadius: 10,
+        top: '20%',
+        width: '100%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        borderColor: 'red',
+        zIndex: 1,  
     },
-    text: {
-        position: 'absolute',
-        left: '32%',
-        top: '10%',
-        fontStyle: 'normal',
-        fontWeight: '400',
-        fontSize: 20,
-        lineHeight: 36,
-        textAlign: 'center',
-        color: '#000000',
-        padding: 15,
+    capsuleImage: {
+        height: '65%',
+        width: '80%'
     },
-    capsuleContainer: {
+    overlayButton: {
+        backgroundColor: 'transparent',
         padding: 10,
+        marginTop: '-70%',
+        width: '80%',
+        height: '65%',
+        zIndex: 2,
+    },
+    overlayText: {
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    capsuleList: {
+        position: 'absolute',
+        top: '20%',
+        width: '100%',
+        height: '50%',
+        backgroundColor: '#8E8E8E',
+        borderRadius: 20,
     },
 });
